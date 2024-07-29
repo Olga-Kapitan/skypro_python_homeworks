42c4c3f8dc48c325e546251f699f475e0b125963
Olga-Kapitan <toy_lulu@bk.ru>
2024-07-29 20:54:15 +0200

    add sixth homework

---
+++ lesson6/lesson_6_task_3.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+browser = webdriver.Chrome()
+browser.get(
+    "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+
+waiter = WebDriverWait(browser, 50)
+
+waiter.until(
+    EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#text"), "Done!"))
+
+src = browser.find_element(By.CSS_SELECTOR, "#award").get_attribute("src")
+
+print(src)
+
+browser.quit
